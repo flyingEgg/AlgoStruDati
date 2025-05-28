b8ee0e36d751476b5cbc9319ec05a84655825f2d
giaco <giaco14mw@gmail.com>
2025-05-28 15:32:48 +0200

    modifica data e codice

--- Prove/coda_stringhe/coda_stringhe.c
+++ Prove/coda_stringhe/coda_stringhe.c
@@ -1,3 +1,5 @@
+/* Prova del 13-09-2023 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,14 +10,14 @@ typedef struct elem_lista{
     struct elem_lista *next_p;
 } elem_lista_t;
 
-int menu(void);
+int menu(elem_lista_t **ingresso, elem_lista_t **uscita);
 char *genera_stringa(void);
 void accoda(elem_lista_t **ingresso, elem_lista_t **uscita, char *valore);
+elem_lista_t *rimuovi(elem_lista_t **ingresso, elem_lista_t **uscita);
 void visita(elem_lista_t *testa);
 
 int main(void){
-    int n, i;
-    char valore[5];
+    int n, i, esito_menu = 1;
     elem_lista_t *testa,
                  *coda;
     testa = coda = NULL;
@@ -26,38 +28,51 @@ int main(void){
     scanf("%d", &n);
 
     for(i = 0; i < n; i++){
-        strcpy(valore, genera_stringa());
-        accoda(&coda, &testa, valore);
+        accoda(&coda, &testa, genera_stringa());
     }
 
     visita(testa);
 
+    do
+        esito_menu = menu(&coda, &testa);
+    while(esito_menu != 0);
+
     return(0);
 }
 
-int menu(void){
-    int scelta,
-        esito;
+int menu(elem_lista_t **ingresso, elem_lista_t **uscita){
+    int esito, scelta;
+    char rimosso[5];
     
-    printf("\nMENU\n
-            1. Inserisci\n
-            2. Rimuovi\n
-            3. Esci\n");
     
+    printf("\n1) Inserisci\n2) Rimuovi\n3) Stop esecuzione\n");
+    scanf("%d", &scelta);
+
     switch(scelta){
         case 1:
-            
+            esito = 1;
+            accoda(ingresso, uscita, genera_stringa());                 /* Continua l'inserimento, esito = 1 --> procede con l'esecuzione */
+            visita((*uscita));
             break;
-        case 2:
+        case 2:                                                       /* Rimuove un elemento, esito = 1 --> procede con l'esecuzione */
+            strcpy(rimosso, rimuovi(ingresso, uscita) -> dato);
+            if(rimosso != NULL){
+                printf("\nElemento rimosso: %s", rimosso);
+                visita((*uscita));
+            }
+            esito = 1;
             break;
-        case 3:
+        case 3:                                                       /* Segnale di stop, esito = 0 --> termina l'esecuzione */
+            esito = 0;                                                  
             break;
         default:
+            printf("Scelta inesistente. Riprovare.\n");
+            esito = 1;
             break;
     }
     
     
-    return (0);
+    return (esito);
 }
 
 char *genera_stringa(void){
@@ -98,4 +113,20 @@ void visita(elem_lista_t *testa){
         corr_p = corr_p -> next_p){
         printf("%s\n", corr_p -> dato);
     }
+}
+
+elem_lista_t *rimuovi(elem_lista_t **ingresso_p, elem_lista_t **uscita_p){
+    elem_lista_t *rimo_p;
+
+    rimo_p = *uscita_p;
+
+    if(*uscita_p != NULL){
+        *uscita_p = (*uscita_p) -> next_p;
+
+        if(*uscita_p == NULL)
+            *ingresso_p = NULL;
+    }
+    
+
+    return (rimo_p);
 }
